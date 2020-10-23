c73d5415ac61659d6c345e4789aa7cfde5a24e19
Roelof Noort <roelofnoort@yandex.com>
2020-10-23 12:37:11 +0200

    TS > JS

--- routers/thatapiguy.js
+++ routers/thatapiguy.js
@@ -1,18 +1,18 @@
 import { Context } from "https://deno.land/x/abc@v1.1.0/mod.ts";
-import type { Group } from "https://deno.land/x/abc@v1.1.0/mod.ts";
-import { logger } from "../utils/logger.ts";
+// import type { Group } from "https://deno.land/x/abc@v1.1.0/mod.ts";
+import { logger } from "../utils/logger.js";
 
 const keys = JSON.parse(Deno.readTextFileSync('./config.json'));
 
 
-export default function (g: Group) {
-    g.get("/dog", async(c: Context) => {
+export default function (g) {
+    g.get("/dog", async(c) => {
         let start = new Date().getTime();
         let body = await fetch("https://api.thedogapi.com/v1/images/search?api_key=" + keys.dogapi);
         logger.debug('Dog took: ' + (new Date().getTime() - start) + "ms");
         return body.json();
     });
-    g.get("/cat", async(c: Context) => {
+    g.get("/cat", async(c) => {
         let start = new Date().getTime();
         let body = await fetch("https://api.thecatapi.com/v1/images/search?api_key=" + keys.catapi);
         logger.debug('Cat took: ' + (new Date().getTime() - start) + "ms");
