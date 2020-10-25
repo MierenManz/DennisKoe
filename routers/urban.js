d4494af11624e544546326b151763d7b73a2eafd
mierenmanz <you@example.com>
2020-10-25 13:42:27 +0100

    removed try catch

--- routers/urban.js
+++ routers/urban.js
@@ -4,19 +4,15 @@ import { Context } from "https://deno.land/x/abc@v1.1.0/mod.ts";
 
 export default function (router) {
     router.get('/:word', async(c) => {
-        try {
-            let res = await fetch(`http://api.urbandictionary.com/v0/define?term=${c.params.word}`)
-            if(!res) return null
-            let body = await res.text()
-            body = await JSON.parse(body)
-            return {
-                def: body.list[0].definition.split("[").join("").split("]").join(""),
-                link: body.list[0].permalink,
-                author: body.list[0].author,
-                date: body.list[0].written_on
-            };
-        } catch(e) {
-            console.log(e)
-        }
+        let res = await fetch(`http://api.urbandictionary.com/v0/define?term=${c.params.word}`)
+        if(!res) return null
+        let body = await res.text()
+        body = await JSON.parse(body)
+        return {
+            def: body.list[0].definition.split("[").join("").split("]").join(""),
+            link: body.list[0].permalink,
+            author: body.list[0].author,
+            date: body.list[0].written_on
+        };
     })
 }
