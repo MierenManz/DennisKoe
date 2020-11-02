2ab553bbf8a0ffa9b89a6bb959e2bd389b06c061
Roelof Noort <roelofnoort@yandex.com>
2020-11-02 21:53:05 +0100

    Kleine changes

--- routers/thatapiguy.js
+++ routers/thatapiguy.js
@@ -1,6 +1,6 @@
-import { Context } from "https://deno.land/x/abc@v1.1.0/mod.ts";
+// import { Context } from "https://deno.land/x/abc@v1.1.0/mod.ts";
 // import type { Group } from "https://deno.land/x/abc@v1.1.0/mod.ts";
-// import { logger } from "../utils/logger.js";
+import { logger } from "../utils/logger.js";
 
 const keys = JSON.parse(Deno.readTextFileSync('./keys.json'));
 
@@ -8,14 +8,26 @@ const keys = JSON.parse(Deno.readTextFileSync('./keys.json'));
 export default function (router) {
     router.get("/doggo", async(c) => {
         let start = new Date().getTime();
-        let body = await fetch("https://api.thedogapi.com/v1/images/search?api_key=" + keys.dogapi);
-        console.log('Dog took: ' + (new Date().getTime() - start) + "ms");
+        let body;
+        try {
+            body = await fetch("https://api.thedogapi.com/v1/images/search?api_key=" + keys.dogapi);
+        } catch(e) {
+            logger.error(e);
+            return { error: "DogAPI is offline"};
+        }
+        logger.debug('Dog took: ' + (new Date().getTime() - start) + "ms");
         return body.json();
     });
     router.get("/catto", async(c) => {
         let start = new Date().getTime();
-        let body = await fetch("https://api.thecatapi.com/v1/images/search?api_key=" + keys.catapi);
-        console.log('Cat took: ' + (new Date().getTime() - start) + "ms");
+        let body;
+        try {
+            body = await fetch("https://api.thecatapi.com/v1/images/search?api_key=" + keys.catapi);
+        } catch(e) {
+            logger.error(e);
+            return { error: "CatAPI is offline"};
+        }
+        logger.debug('Cat took: ' + (new Date().getTime() - start) + "ms");
         return body.json();
     });
 }
--- routers/urban.js
+++ routers/urban.js
@@ -1,18 +1,19 @@
-import { Context } from "https://deno.land/x/abc@v1.1.0/mod.ts";
+// import { Context } from "https://deno.land/x/abc@v1.1.0/mod.ts";
 // import type { Group } from "https://deno.land/x/abc@v1.1.0/mod.ts";
-// import { logger } from "../utils/logger.js";
+import { logger } from "../utils/logger.js";
 
 export default function (router) {
     router.get('/:word', async(c) => {
+        let start = new Date().getTime();
         let res = await fetch(`http://api.urbandictionary.com/v0/define?term=${c.params.word}`);
         if(!res) return null;
-        let body = await res.text();
-        body = await JSON.parse(body);
+        let body = await JSON.parse(await res.text());
+        logger.debug('Urban took: ' + (new Date().getTime() - start) + "ms");
         return {
             def: body.list[0].definition.split("[").join("").split("]").join(""),
             link: body.list[0].permalink,
             author: body.list[0].author,
             date: body.list[0].written_on
         };
-    })
+    });
 }
