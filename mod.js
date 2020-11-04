b8eca53c305c14d662de0cb08874d6b0e275e6ad
Roelof Noort <roelofnoort@yandex.com>
2020-11-04 12:10:57 +0100

    Twee nieuwe routers toegevoegd, reddit werkt nog niet.

--- mod.js
+++ mod.js
@@ -6,9 +6,13 @@ const config = JSON.parse(Deno.readTextFileSync('./config.json'));
 
 import thatapiguyRouter from "./routers/thatapiguy.js";
 import urbanRouter from "./routers/urban.js";
+import redditRouter from "./routers/reddit.js";
+import nekobotRouter from "./routers/nekobot.js";
 
 thatapiguyRouter(app.group("thatapiguy"));
 urbanRouter(app.group("urban"));
+redditRouter(app.group("reddit"));
+nekobotRouter(app.group("nekobot"));
 
 app.start({ port: config.port });   
 logger.info('Listening on port ' + config.port);
