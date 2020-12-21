c8b072c479ce9ab79458739e2ea180b3261bde1c
mierenmanz <christiaanmens@gmail.com>
2020-12-21 22:36:58 +0100

    changed types

--- routers/thatapiguy.ts
+++ routers/thatapiguy.ts
@@ -15,18 +15,18 @@
  * 
  */
 
-// import all features
+// import all types & classes
 import { Router } from "https://deno.land/x/oak@v6.4.0/mod.ts";
-import {Keys, ContextWithParams, Body} from "../utils/types.ts";
+import { ApiKeys, TagContext, Body } from "../utils/types.ts";
 
 // read keys for request
-const keys:Keys = await JSON.parse(await Deno.readTextFile('./keys.json'))
+const keys:ApiKeys = await JSON.parse(await Deno.readTextFile('./keys.json'))
 
 // create new router
 const router = new Router({prefix: "/tag"})
 
 // router for ThatApiGuy (TAG)
-router.get('/:id', async (ctx:ContextWithParams) => {
+router.get('/:id', async (ctx:TagContext) => {
     const api: string = ctx.params.id.substr(0, 3);
     if (api === "cat" || api === "dog") {
         let body: Body = await (await fetch(`https://api.the${api}api.com/v1/images/search?api_key=${keys[api]}`)).json();
