bfc18b86a167399262aa131202cde27e1f54a23b
mierenmanz <christiaanmens@gmail.com>
2020-12-21 22:35:49 +0100

    merged logger and tracker

--- utils/tracker.ts
+++ utils/tracker.ts
@@ -15,7 +15,9 @@
  * 
  */
 import { Context } from "https://deno.land/x/oak@v6.4.0/context.ts";
-import { logger } from "../utils/logger.ts";
+import { Logger } from "https://deno.land/x/optic@1.0.2/mod.ts";
+
+const logger = new Logger();
 
 export function tracker(c:Context): void {
     logger.debug(`Tracker: [${c.request.method}]: [${c.request.ip}] => [${c.request.url}]`);
