77b1f2612859b1c8d9bb141721a6e5908e7b170d
mierenmanz <christiaanmens@gmail.com>
2020-12-21 22:56:52 +0100

    added testing folder and filesystem tests

---
+++ testing/filesystem/test.js
@@ -0,0 +1,21 @@
+import { FileSystem } from "../../utils/filesystem.ts";
+
+Deno.test({
+    name: 'FS FileExist',
+    fn: FE
+})
+Deno.test({
+    name: 'FS FileExistSync',
+    fn: FES
+})
+Deno.chdir(Deno.cwd() + "\\testing\\filesystem");
+
+async function FE() {
+    console.log("\nthis should be false:", await FileSystem.fileExist('ree.txt'));
+    console.log("this should be true:", await FileSystem.fileExist('thisexists.txt'));
+}
+
+function FES() {
+    console.log("\nthis should be false:", FileSystem.fileExistSync('ree.txt'));
+    console.log("this should be true:", FileSystem.fileExistSync('thisexists.txt'));
+}
