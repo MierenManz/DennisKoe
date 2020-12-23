dfb5162a832e6150c79901a44d1ffbfd9b935cdd
mierenmanz <christiaanmens@gmail.com>
2020-12-23 23:03:21 +0100

    fixed tests for fs

--- testing/filesystem/test.js
+++ testing/filesystem/test.js
@@ -1,23 +1,22 @@
-import { FileSystem } from "../../utils/filesystem.ts";
+import { fileExist, fileExistSync } from "../../utils/filesystem.ts";
 
 Deno.test({
     name: 'FS FileExist',
-    //only: true,
     fn: FE
 });
 Deno.test({
     name: 'FS FileExistSync',
-    //only: true,
     fn: FES
 });
+
 Deno.chdir(Deno.cwd() + "\\testing\\filesystem");
 
 async function FE() {
-    console.log("\nthis should be false:", await FileSystem.fileExist('ree.txt'));
-    console.log("this should be true:", await FileSystem.fileExist('thisexists.txt'));
+    console.log("\nthis should be false:", await fileExist('ree.txt'));
+    console.log("this should be true:", await fileExist('thisexists.txt'));
 }
 
 function FES() {
-    console.log("\nthis should be false:", FileSystem.fileExistSync('ree.txt'));
-    console.log("this should be true:", FileSystem.fileExistSync('thisexists.txt'));
+    console.log("\nthis should be false:", fileExistSync('ree.txt'));
+    console.log("this should be true:", fileExistSync('thisexists.txt'));
 }
