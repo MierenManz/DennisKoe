8c4e056f4bf96c0a1efbe9f97e878b4f4492fe5d
mierenmanz <christiaanmens@gmail.com>
2020-12-23 23:04:32 +0100

    splitted logger and tracker again

---
+++ utils/logger.ts
@@ -0,0 +1,18 @@
+/*
+ * 
+ *        :::::::::  
+ *       :+:    :+:  
+ *      +:+    +:+  
+ *     +#+    +:+   
+ *    +#+    +#+    
+ *   #+#    #+#     
+ *  #########   enniskoe
+ * 
+ *  logger.ts
+ * 
+ *  This file belongs to Denniskoe
+ *  Dit bestand behoort tot Denniskoe
+ * 
+ */
+import { Logger } from "https://deno.land/x/optic@1.0.2/mod.ts";
+export const logger = new Logger();
