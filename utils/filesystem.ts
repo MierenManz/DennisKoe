ac5525c58256e6c4db301c962cfbe90597bc9bf1
mierenmanz <christiaanmens@gmail.com>
2020-12-23 23:03:11 +0100

    changed Filesystem.ts from namespace to seperate functions

--- utils/filesystem.ts
+++ utils/filesystem.ts
@@ -8,28 +8,27 @@
  *   #+#    #+#     
  *  #########   enniskoe
  * 
- *  exist.ts
+ *  filesystem.ts
  * 
  *  This file belongs to Denniskoe
  *  Dit bestand behoort tot Denniskoe
  * 
  */
 
-export namespace FileSystem {
-    export async function fileExist(filePath:string): Promise<boolean> {
-        try {
-            await Deno.stat(filePath);
-        } catch(e) {
-            return false;
-        }
-        return true;
+export async function fileExist(filePath:string): Promise<boolean> {
+    try {
+        await Deno.stat(filePath);
+    } catch(e) {
+        return false;
     }
-    export function fileExistSync(filePath:string): boolean {
-        try {
-            Deno.statSync(filePath);
-        } catch(e) {
-            return false;
-        }
-        return true;
+    return true;
+}
+
+export function fileExistSync(filePath:string): boolean {
+    try {
+        Deno.statSync(filePath);
+    } catch(e) {
+        return false;
     }
+    return true;
 }
