13f1ba7af7a8a778fc72e7353ae60c1a8aa25951
mierenmanz <christiaanmens@gmail.com>
2020-12-23 23:04:43 +0100

    removed async from test

--- routers/test.ts
+++ routers/test.ts
@@ -20,7 +20,7 @@ import { Router, Context } from "https://deno.land/x/oak@v6.4.0/mod.ts";
 
 const router = new Router({prefix: "/test"})
 
-router.get('/ree', async (ctx:Context) => {
+router.get('/ree', (ctx:Context) => {
     ctx.response.body = "Hello from testing!";
     return;
 })
