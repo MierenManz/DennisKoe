2fd347704198d3eb137a2c2d91a29f208d61e47d
mierenmanz <christiaanmens@gmail.com>
2020-12-25 19:00:35 +0100

    formatting

--- mod.ts
+++ mod.ts
@@ -20,17 +20,18 @@ import { Application, Context } from "https://deno.land/x/oak@v6.4.0/mod.ts";
 import { bold, brightGreen, magenta } from "https://deno.land/std@0.82.0/fmt/colors.ts";
 import { tracker } from "./utils/tracker.ts";
 import { logger } from "./utils/logger.ts";
-import { ServerConfig } from "./utils/types.ts";
+import { config } from "./utils/common.ts";
 
-// Read ServerConfig
-const config: ServerConfig = await JSON.parse(await Deno.readTextFile("./config.json"));
 
 //import all routers
 import thatApiGuyRouter from "./routers/thatapiguy.ts";
 import customRouter from "./routers/custom.ts";
 import nekobotRouter from  "./routers/nekobot.ts";
+
+
 // create new app
 const app = new Application();
+
 
 // add tracker
 app.use(async (ctx: Context, next) => {
@@ -39,6 +40,7 @@ app.use(async (ctx: Context, next) => {
     return;
 });
 
+
 //add all routers
 app.use(thatApiGuyRouter.routes());
 app.use(thatApiGuyRouter.allowedMethods());
@@ -46,6 +48,8 @@ app.use(customRouter.routes());
 app.use(customRouter.allowedMethods());
 app.use(nekobotRouter.routes());
 app.use(nekobotRouter.allowedMethods());
+
+
 //start server
 logger.info(`${magenta("Listening")}  ${bold("[")}${brightGreen(String(config.server.port))}${bold("]")}`);
 await app.listen({ port: config.server.port });
