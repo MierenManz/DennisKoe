b9bb9cca3b7f735e9e5ffffbf3820e090a151a8d
mierenmanz <christiaanmens@gmail.com>
2020-12-26 13:42:15 +0100

    Fixed bug: Cache dir now get's made

--- mod.ts
+++ mod.ts
@@ -15,25 +15,25 @@
  * 
  */
 
-// import oak app
+// Import oak app
 import { Application, Context } from "https://deno.land/x/oak@v6.4.0/mod.ts";
 import { bold, brightGreen, magenta } from "https://deno.land/std@0.82.0/fmt/colors.ts";
 import { tracker } from "./utils/tracker.ts";
 import { logger } from "./utils/logger.ts";
 import { config } from "./utils/common.ts";
+import { fileExist } from "./utils/filesystem.ts";
 
-
-//import all routers
+// Import all routers
 import thatApiGuyRouter from "./routers/thatapiguy.ts";
 import ffmpegRouter from "./routers/ffmpeg.ts";
 import nekobotRouter from  "./routers/nekobot.ts";
 import statusRouter from "./routers/status.ts";
 
-// create new app
+// Create new app
 const app = new Application();
 
 
-// add tracker
+// Add tracker
 app.use(async (ctx: Context, next) => {
     if (String(ctx.request.url).includes("favicon") === false) tracker(ctx);
     await next();
@@ -41,7 +41,7 @@ app.use(async (ctx: Context, next) => {
 });
 
 
-//add all routers
+// Add all routers
 app.use(thatApiGuyRouter.routes());
 app.use(thatApiGuyRouter.allowedMethods());
 app.use(ffmpegRouter.routes());
@@ -52,6 +52,13 @@ app.use(statusRouter.routes());
 app.use(statusRouter.allowedMethods());
 
 
-//start server
+// Cache Directory
+const cache = `${Deno.cwd()}/cache/`;
+
+// Make cache dir if it doesn't exist
+if (await fileExist(cache) === false) await Deno.mkdir(cache);
+
+
+// Start server
 logger.info(`${magenta("Listening")}  ${bold("[")}${brightGreen(String(config.server.port))}${bold("]")}`);
 await app.listen({ port: config.server.port });
