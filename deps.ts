6e84ed284420e0f1a0a32ee8c240b3976463137e
mierenmanz <christiaanmens@gmail.com>
2021-01-23 22:11:00 +0100

    updated deps

--- deps.ts
+++ deps.ts
@@ -14,7 +14,7 @@
  *  Dit bestand behoort tot Denniskoe
  * 
  */
-export * from "https://deno.land/x/oak@v6.4.0/mod.ts";
-export * from "https://deno.land/std@0.82.0/fmt/colors.ts";
-export * from "https://deno.land/x/optic@1.0.2/mod.ts";
-export * from "https://deno.land/x/deno_ffmpeg@2.0/mod.ts";
+export * from "https://deno.land/x/oak@v6.4.2/mod.ts";
+export * from "https://deno.land/std@0.84.0/fmt/colors.ts";
+export * from "https://deno.land/x/optic@1.2.2/mod.ts";
+export * from "https://deno.land/x/deno_ffmpeg@2.1.0/mod.ts";
