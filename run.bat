853e4a43c294afcb310ad7e55a5dbb820661557d
MierenManz <christiaanmens@gmail.com>
2021-07-05 21:40:10 +0200

    fix shell commands

--- run.bat
+++ run.bat
@@ -1,1 +1,6 @@
-deno run --allow-write=./cache/crabbovids --allow-read=./ --allow-net=0.0.0.0:6969,api.thedogapi.com,api.thecatapi.com,api.urbandictionary.com,quotes15.p.rapidapi.com --allow-run=ffmpeg,./ffmpeg/ffmpeg mod.ts
+deno run \
+--allow-write=./logs/ \
+--allow-read=./config.json,./keys.json \
+--allow-net=0.0.0.0:6969,api.thedogapi.com,api.thecatapi.com,api.urbandictionary.com,quotes15.p.rapidapi.com \
+--allow-run=ffmpeg,./ffmpeg/ffmpeg,./ffmpeg/ffmpeg.exe \
+mod.ts
